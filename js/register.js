e69568a9b97fe3dcf92011571fed34e3d2bc7b23
spacanowski <spacanowski@gmail.com>
2016-04-11 21:20:44 +0200

    Added confirmation page

--- js/register.js
+++ js/register.js
@@ -86,3 +86,27 @@ $(document).ready(function() {
             return false;
         });
 });
+
+function confirm(meal) {
+    $('#throbber').show();
+    jQuery.ajax({
+        'type': 'POST',
+        'url': "http://superchicken-devcrowd.rhcloud.com/participants/" + getIdFromUrl() + "/confirmed",
+        'contentType': 'application/json',
+        'accept': 'application/json',
+        'data': JSON.stringify(meal),
+        'success': function( data ) {
+            $('#throbber').hide();
+            window.location = "regsuc.html#greatSuccess";
+        },
+        'error': function( data ) {
+            $("#failModal").modal();
+            $('#throbber').hide();
+        }
+    });
+}
+function getIdFromUrl() {
+    var regex = new RegExp("[?]id=([a-zA-Z0-9\-]*)", "i"),
+        results = regex.exec(window.location.href);
+    return decodeURIComponent(results[1]);
+}
